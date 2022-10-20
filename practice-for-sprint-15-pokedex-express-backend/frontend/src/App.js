64551313e029bceaf60e9575a5de501e8c32fad8
Mitchell Jindra <mitchjindra413@gmail.com>
2022-10-20 14:41:24 -0700

    add frontend

---
+++ practice-for-sprint-15-pokedex-express-backend/frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Route, Switch } from "react-router-dom";
+
+import PokemonBrowser from "./components/PokemonBrowser";
+
+const App = () => {
+  return (
+    <Switch>
+      <Route
+        path={["/", "/pokemon", "/pokemon/:pokemonId"]}
+        exact
+      >
+        <PokemonBrowser />
+        
+      </Route>
+    </Switch>
+  );
+};
+
+export default App;
